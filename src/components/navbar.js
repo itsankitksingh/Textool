34a8fd3cedaf7e30e9c8713e92c8d80e31699e9e
Ankit <itsankitksingh@gmail.com>
2023-09-28 11:05:51 +0530

    my first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Navbar(props) {
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <a className="navbar-brand" href="/">{props.title}</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          </li>
+          {/* <li className="nav-item">
+            <a className="nav-link" href="/">{props.about}</a>
+          </li> */}
+        </ul>
+        
+        <div className={`form-check form-switch text-${props.mode==='light'? 'dark':'light'}`}>
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+  about: PropTypes.string,
+  mode: PropTypes.string.isRequired, // You should include the mode prop and specify it as required
+  toggleMode: PropTypes.func.isRequired, // You should include the toggleMode prop and specify it as required
+};
