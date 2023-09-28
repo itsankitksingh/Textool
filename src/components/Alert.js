a6eebb63485dd05fb0b693d55b6fcef545afa765
Ankit Kumar Singh <97602488+itsankitksingh@users.noreply.github.com>
2023-09-28 16:32:51 +0530

    Alert Shifting improved

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,10 +9,12 @@ function Alert(props) {
     }
 
     return (
-        props.alert &&
+        <div  style={{height:'50px'}}>   
+       { props.alert &&
        // <div className={`alert alert-success alert-dismissible fade show`} role="alert">
         <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{ capitilise(props.alert.type)}</strong> : {props.alert.msg}
+        </div>}
         </div>
     )
 }
